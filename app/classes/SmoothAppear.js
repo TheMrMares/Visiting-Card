91acb382c2c9c12685d56ad70bc4fabb105d6498
Unknown <rak.bartosz98@gmail.com>
2018-07-24 14:34:27 +0200

    Growing numbers effect
    Created effect that animates numbers values when user see them, because they are in viewport.

--- app/classes/SmoothAppear.js
+++ app/classes/SmoothAppear.js
@@ -1,10 +1,11 @@
 import 'jquery';
 import 'jquery-ui';
+import anime from 'animejs';
 
 export class SmoothAppear {
     constructor(context, delay, borderStart, borderEnd){
         this.context = context;
-        this.elem = [];
+        this.appearElem = [];
         let allElem = [
             ...context.querySelectorAll('p'),
             ...context.querySelectorAll('h1'),
@@ -17,33 +18,73 @@ export class SmoothAppear {
             ...context.querySelectorAll('.group__marker')
         ];
         allElem.forEach((item, index) => {
-            this.elem.push({
+            this.appearElem.push({
                 obj: item,
                 visible: false
             });
         })
+        this.growElem = [];
+        document.querySelectorAll('.grownumber').forEach((item, index) => {
+            this.growElem.push({
+                obj: item,
+                visible: false
+            });
+        });
+        console.log(this.growElem);
         this.borderStart = borderStart;
         this.borderEnd = borderEnd;
         this.delay = delay;
     }
+    inViewport(item){
+        let rect = item.getBoundingClientRect();
+        if(rect.top >= 0 + this.borderStart && rect.bottom <= window.innerHeight - this.borderEnd){
+            return true;
+        } else {
+            return false;
+        }
+    }
     check(){
         let duration = 300;
-        this.elem.forEach((item, index) => {
-            let rect = item.obj.getBoundingClientRect();
-            if(rect.top >= 0 + this.borderStart && rect.bottom <= window.innerHeight - this.borderEnd){
+        this.appearElem.forEach((item, index) => {
+            if(this.inViewport(item.obj)){
                 if(item.visible == false){
                     $(item.obj).delay(this.delay).animate({
                         opacity: 1
                     },duration);
+                    item.visible = true;
                 }
-                item.visible = true;
             } else {
                 if(item.visible == true){
                     $(item.obj).delay(this.delay).animate({
                         opacity: 0.1
                     },duration);
+                    item.visible = false;
                 }
-                item.visible = false;
+            }
+        });
+        this.growElem.forEach((item, index) => {
+            console.log('xd');
+            if(this.inViewport(item.obj)){
+                console.log(item.obj);
+                if(item.visible == false){
+                    anime({
+                        targets: item.obj,
+                        textContent: `${item.obj.getAttribute('sb-grow')}%`,
+                        round: 1,
+                        easing: 'easeInOutExpo'
+                    });
+                    item.visible = true;
+                }
+            } else {
+                if(item.visible == true){
+                    anime({
+                        targets: item.obj,
+                        textContent: `${0}%`,
+                        round: 1,
+                        easing: 'easeInOutExpo'
+                    });
+                    item.visible = false;
+                }
             }
         });
     }
